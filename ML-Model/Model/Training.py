33e2b0f8034b0687ee59d087935b6f5ca9a9418f
Shiv Gowda <151723225+Shiv-61@users.noreply.github.com>
2026-01-26 11:22:09 +0530

    Commit 3: Add Training.py model script

---
+++ ML-Model/Model/Training.py
@@ -0,0 +1,7 @@
+﻿import pandas as pd
+from sklearn.model_selection import train_test_split
+from tensorflow import keras
+
+# Load dataset
+data = pd.read_csv('Dataset/new_aqi.csv')
+print("Dataset loaded successfully")
