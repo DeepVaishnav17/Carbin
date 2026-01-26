0e9ad6bf5f04d26b8d5382e589cc28de8940052d
Shiv Gowda <151723225+Shiv-61@users.noreply.github.com>
2026-01-26 11:22:09 +0530

    Commit 4: Add Flask app.py

---
+++ ML-Model/src/app.py
@@ -0,0 +1,13 @@
+﻿from flask import Flask, jsonify, request
+from model_defs import predict_aqi
+
+app = Flask(__name__)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.json
+    result = predict_aqi(data)
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
