3f595ee15d9f40f51e09908d8a5d31337433cf77
Shiv Gowda <151723225+Shiv-61@users.noreply.github.com>
2026-01-26 11:22:09 +0530

    Commit 5: Add model_defs.py with prediction functions

---
+++ ML-Model/src/model_defs.py
@@ -0,0 +1,10 @@
+﻿import numpy as np
+from sklearn.ensemble import RandomForestRegressor
+
+def predict_aqi(features):
+    model = RandomForestRegressor(n_estimators=100)
+    prediction = model.predict([features])
+    return {'aqi': float(prediction[0])}
+
+def load_model():
+    return RandomForestRegressor(n_estimators=100)
