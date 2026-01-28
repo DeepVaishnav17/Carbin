5b70ffefe097fc8266ac778e98b96862d528becb
DeepVaishnav17 <deepvaishnav207@gmail.com>
2026-01-28 18:37:21 +0530

    Fix email send timeout with port 465

--- carbon-backend/src/utils/sendEmail.js
+++ carbon-backend/src/utils/sendEmail.js
@@ -3,11 +3,15 @@ const nodemailer = require("nodemailer");
 const sendEmail = async (to, subject, html) => {
   try {
     const transporter = nodemailer.createTransport({
-      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true, // true for 465, false for other ports
       auth: {
         user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS, // Gmail App Password
+        pass: process.env.EMAIL_PASS,
       },
+      // connectionTimeout: 10000, // 10 seconds
+      // socketTimeout: 10000,     // 10 seconds
     });
 
     const info = await transporter.sendMail({
